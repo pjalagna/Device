aa1bc243d72c9a7c4c178ab875dc5c384b477e4d
pjalagna <pjalagna@gmail.com>
2017-01-13 23:15:39 -0500

    edits for TCVRClass

--- DeviceDriverSnd.py
+++ DeviceDriverSnd.py
@@ -2,6 +2,17 @@
 DeviceDriverSnd(port,data):
     """ this driver is a stump function that writes to the database """
     import TCVRClass
-    db1 = TCVRClass.TCVR()
-    db1.TCVRWrite(T='PORT',C='Byte',V=data,R=port)
+    db1 = TCVRClass.TCVR('hubdb')
+    j = db1.newRec()
+    j[db._TABLE] = 'PORT'
+    j[db._COLUMN] = 'Byte'
+    j[db._ROW] = port
+    j[db._VALUE] = data
+    try:
+        db1.update(j)
+    except:
+        db1.insert(j)
+    finaly:
+        nop = -1
+    #end try
 #end DeviceDriverSnd
